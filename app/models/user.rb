35d6671358ad745eb004151f85210c06e07ec736
yuuki10510 <ikedayuki@ikedayukinoMacBook-Air.local>
2026-01-22 23:10:37 +0900

    add/ロール管理用のユーザーモデルを作成

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { admin: 0, parent: 1, student: 2 }
+
+  has_one :student_profile, dependent: :destroy
+  has_many :parent_students, foreign_key: :parent_id
+  has_many :children, through: :parent_students, source: :student
+
+  has_many :scores, foreign_key: :student_id
+end
