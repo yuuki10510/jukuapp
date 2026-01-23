c2508e2e5258aa1b8f8c64d6257b57e9fc131aec
yuuki10510 <ikedayuki@ikedayukinoMacBook-Air.local>
2026-01-23 17:40:27 +0900

    add/保護者と生徒用の新規登録のviewを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get "announcements/index"
   get "announcements/show"
   get "scores/index"
-  devise_for :users
+  devise_for :users, controllers: { registrations: "users/registrations" }
 
   root "dashboards#home"
 
