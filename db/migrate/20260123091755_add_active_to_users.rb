a0b0ce6b0853284c1832a4738da05b964f6a6464
yuuki10510 <ikedayuki@ikedayukinoMacBook-Air.local>
2026-01-24 18:09:09 +0900

    feat/管理画面viewの実装

---
+++ db/migrate/20260123091755_add_active_to_users.rb
@@ -0,0 +1,5 @@
+class AddActiveToUsers < ActiveRecord::Migration[8.1]
+  def change
+    add_column :users, :active, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_22_152805) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_23_091755) do
   create_table "announcements", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_22_152805) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "active"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
