7136151a05a992f6b8f9333a878a4454c3301e0b
yuuki10510 <ikedayuki@ikedayukinoMacBook-Air.local>
2026-01-27 15:44:12 +0900

    feat/退塾時にユーザーを無効化

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -1,12 +1,22 @@
 class Admin::StudentsController < Admin::BaseController
 
   def index
-    @students = User.student.includes(:student_profile)
+    @students = User.student.includes(:student_profile).where(active: true)
     @students = @students.where(student_profiles: { grade: params[:grade] }) if params[:grade].present?
   end
 
   def show
     @student = User.student.find(params[:id])
     @scores = @student.scores
+  end
+
+  def withdraw
+    @student = User.student.find(params[:id])
+
+    if @student.update(active: false, withdrawal_reason: params[:withdrawal_reason])
+      redirect_to admin_students_path, notice: "退塾処理が完了しました。"
+    else
+      render :show
+    end
   end
 end
